fa32cb9592b67116b55411cbd0b1eff3eb8196cd
cjsiador <54792076+cjsiador@users.noreply.github.com>
2024-07-26 23:47:42 -1000

    Created Landing Page

--- SpeakEasy/src/components/LandingComp.jsx
+++ SpeakEasy/src/components/LandingComp.jsx
@@ -1,10 +1,31 @@
 import { useEffect, useRef, useState } from 'react'
+import speakEasyLogo from '../assets/logo-no-background.svg'
+import { Button } from '@chakra-ui/react'
 
 function LandingComp() {
 
     return(
         <>
-        
+            <div className='hero-container'>
+                <img src={speakEasyLogo}></img>
+                {/* <h1>SpeakEasy</h1>
+                <h2>Unlock the World with Your Voice</h2> */}
+                <Button
+                    fontSize='20px'
+                    size='md'
+                    height='48px'
+                    width='200px'
+                    border='4px'
+                    borderColor='white'
+                    colorScheme= 'transparent'
+                    _hover={{ 
+                        borderColor: '#ffffff',
+                        opacity: '80%'
+                    }}
+                >
+                    GET STARTED
+                </Button>
+            </div>
         </>
     )
     
---
+++ SpeakEasy/src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+import { useEffect, useRef, useState } from 'react'
+import speakEasyLogo from '../assets/logo-no-background.svg'
+import { Flex, Spacer, Box, Button, ButtonGroup } from '@chakra-ui/react'
+
+function NavBar () {
+    return (
+        <>
+            <div className='nav-bar'>
+                <Flex minWidth='max-content' alignItems='center' gap='2'>
+                    <Box p='2' position='relative'>
+                        {/* <img className='nav-bar-img' src={speakEasyLogo}></img> */}
+                        <Button
+                            colorScheme= 'transparent'
+                            m={2}
+                        >
+                            <h1 className='nav-bar-h1'>Speak Easy</h1>
+                        </Button>
+                    </Box>
+                    <Spacer/>
+                    <ButtonGroup p='2' gap='2' className='nav-bar-right-side' style={{marginTop: '10px'}}>
+                        <Button
+                            fontSize='20px'
+                            size='md'
+                            height='48px'
+                            width='100px'
+                            border='4px'
+                            borderColor='white'
+                            colorScheme= 'transparent'
+                            _hover={{ 
+                                borderColor: '#ffffff',
+                                opacity: '80%'
+                            }}
+                        >
+                            Sign Up
+                        </Button>
+                        <Button
+                            fontSize='20px'
+                            size='md'
+                            height='48px'
+                            width='100px'
+                            border='4px'
+                            borderColor='white'
+                            colorScheme= 'transparent'
+                            _hover={{ 
+                                borderColor: '#ffffff',
+                                opacity: '80%'
+                            }}
+                        >
+                            Login
+                        </Button>
+                    </ButtonGroup>
+                </Flex>
+            </div>
+        </>
+    )
+}
+
+export default NavBar
