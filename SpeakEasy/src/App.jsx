1b0fbfe17d3672a8b087457bd9c40e1e63b56a8f
cjsiador <54792076+cjsiador@users.noreply.github.com>
2024-07-27 00:14:50 -1000

    Added Translator

--- SpeakEasy/src/App.jsx
+++ SpeakEasy/src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
 import LandingComp from './components/LandingComp'
 import NavBar from './components/NavBar'
+import TranslationComp from './components/TranslationComp'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <NavBar/>
       <LandingComp/>
+      <TranslationComp />
     </>
   )
 }
